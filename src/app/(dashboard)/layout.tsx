cbb71bcbafeba08af6ca15f53c4bc35253693d70
Nishansanad <nishansanad@gmail.com>
2026-04-21 18:04:42 +0400

    sidebar and navbar set up

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import '../globals.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "School Management Dashboard",
+  description: "Next.js School Management System",
+};
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div className="h-screen flex">
+    {/* LEFT */}
+    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+      <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+      <Image src="/favicon.ico" alt="logo" width={32} height={32}/>
+      <span className="hidden lg:block">SchoolApp</span>
+      </Link>
+      <Menu/>
+    </div>
+    {/* RIGHT */}
+    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      <Navbar />
+      {children}
+    </div>
+    </div>;
+}
