841a9e4c4fbf309e6c5b466ce15ee92485636fbd
Nishansanad <nishansanad@gmail.com>
2026-06-27 18:16:33 +0400

    added react hook form, zod

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import TeacherForm from "./forms/TeacherForm";
 
 const FormModal = ({
   table,
@@ -43,7 +44,7 @@ const FormModal = ({
         <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
       </form>
     ) : (
-      "create or update form"
+      <TeacherForm type="create" />
     );
   };
 
---
+++ src/components/InputField.tsx
@@ -0,0 +1,25 @@
+import { FieldError } from "react-hook-form";
+
+type InputFieldProps = {
+  label: string;
+  type?: string;
+  register: any;
+  name: string;
+  defaultValue: string;
+  error?: FieldError;
+  inputProps: React.InputHTMLAttributes<HTMLInputElement>;
+};
+
+const InputField = ({
+  label,
+  type = "text",
+  register,
+  name,
+  defaultValue,
+  error,
+  inputProps,
+}: InputFieldProps) => {
+  return <div className="">InputField</div>;
+};
+
+export default InputField;
