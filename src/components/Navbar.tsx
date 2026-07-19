59e8288a57011db12ffbd684a2f4c0e2a76a3ba7
Nishansanad <nishansanad@gmail.com>
2026-07-19 22:23:52 +0400

    minor fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             </div>
 
             <div className="flex flex-col">
-                <span className="text-xs leading-3 font-medium">John Doe</span>
+                <span className="text-xs leading-3 font-medium">Nishan Sanad</span>
                 <span className="text-[10px] text-gray-500 text-right">Admin</span>
             </div>
 
