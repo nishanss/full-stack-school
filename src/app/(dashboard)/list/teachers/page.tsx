402b47c03ee1966850b17b5e5068194c1bff9e52
Nishansanad <nishansanad@gmail.com>
2026-08-14 20:50:57 +0400

    minor fixes

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -116,16 +116,17 @@ const TeacherListPage = async ({
     for (const [key, value] of Object.entries(queryParams)) {
       if (value !== undefined) {
         switch (key) {
-          case "classId": {
+          case "classId": 
             query.lessons = {
               some: {
                 classId: parseInt(value),
               },
             };
             break;
-            case "search":
-              query.name = {contains: value, mode: "insensitive"}
-          }
+          
+          case "search": 
+            query.name = { contains: value, mode: "insensitive" }
+          
         }
       }
     }
